f3653c3a9365626c0f426be58e2199113f043856
Zuo Yang <zuya@microsoft.com>
2018-08-24 07:08:19 +0800

    finish "Unit Testing Angular Code"

---
+++ src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,44 @@
+import { SessionListComponent } from './session-list.component';
+import { ISession } from '../shared';
+
+describe('SessionListComponent', () => {
+  let component: SessionListComponent;
+  const mockAuthService;
+  const mockVoterService;
+
+  beforeEach(() => {
+    component = new SessionListComponent(mockAuthService, mockVoterService);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 2', level: 'beginner' },
+        { name: 'session 3', level: 'intermediate' }
+      ];
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = <ISession[]>[
+        { name: 'session 1', level: 'intermediate' },
+        { name: 'session 3', level: 'beginner' },
+        { name: 'session 2', level: 'intermediate' }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions[2].name).toBe('session 3');
+    });
+  });
+});
